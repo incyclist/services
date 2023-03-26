3d78482313c1e89a43945bca451a9673a78131a6
Guido Doumen <incyclist@doumen-laumen.de>
2023-03-26 15:42:38 +0200

    bugfix: clone throws error if object is null or undefined

--- src/utils/clone.ts
+++ src/utils/clone.ts
@@ -6,5 +6,7 @@
  * @param   obj - Object to be cloned 
  */
 export default function clone(obj:any):any {
+    if (obj===null || obj===undefined)
+        return obj
     return JSON.parse(JSON.stringify(obj))
 }
