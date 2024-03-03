f5adcecf05921ba6546b2ae3c3cc15609dff37af
Guido Doumen <incyclist@doumen-laumen.de>
2024-03-03 19:10:59 +0100

    add test case coverage for formatting functions

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -1,6 +1,14 @@
 import { valid } from "./valid";
-
-export const dateFormat = (date:Date, fstr='%Y%m%d%H%M%S', utc:boolean=false) => {
+/**
+ * Formats a Date object into a string using the specified format string.
+ * @param {Date} date - The Date object to format.
+ * @param {string} [fstr='%Y%m%d%H%M%S'] - The format string. Defaults to '%Y%m%d%H%M%S'.
+ * %Y represents year, %m represents month, %d represents day of month, 
+ * %H is hour, %M is minute, %S is seconds
+ * @param {boolean} [utc=false] - Indicates whether to use UTC methods for date extraction. Defaults to false.
+ * @returns {string|undefined} The formatted date string or undefined if the input date is invalid.
+ */
+export const formatDateTime = (date:Date, fstr='%Y%m%d%H%M%S', utc:boolean=false) => {
     if ( !valid(date) || !(date instanceof Date))
         return;
 
@@ -24,3 +32,72 @@ export const dateFormat = (date:Date, fstr='%Y%m%d%H%M%S', utc:boolean=false) =>
         return ('0' + m).slice (-2);
     });
 }
+
+/**
+ * Formats a time duration given in seconds into a string in the format "HH:MM:SS" or "MM:SS".
+ * @param {number} seconds - The time duration in seconds.
+ * @param {boolean} cutMissing - Indicates whether to cut missing leading hours.
+ * @returns {string|undefined} The formatted time string or undefined if seconds is undefined or null.
+ */
+export const formatTime = ( seconds:number, cutMissing:boolean ) =>{
+    if ( seconds===undefined || seconds===null)
+        return;
+
+    let timeVal = seconds>=0 ? Math.round(seconds) : 0;
+
+    const h = Math.floor(timeVal/3600);
+    timeVal = timeVal % 3600;
+    const m = Math.floor(timeVal/60);
+    timeVal = timeVal % 60;
+    const s = Math.floor(timeVal);
+
+    if (cutMissing===undefined || cutMissing===false || h>0)
+        return  h + ':'+ pad(m) +':' +pad(s);
+    else 
+        return pad(m) +':' +pad(s);
+} 
+
+/**
+ * Formats a number into a string with a specified maximum number of digits and maximum length.
+ * @param {number} value - The number to format.
+ * @param {number} maxDigits - The maximum number of digits to include after the decimal point.
+ * @param {number} [maxLength=-1] - The maximum length of the resulting string. Defaults to -1 (no maximum length).
+ * @returns {string} The formatted number string.
+ */
+export const formatNumber = ( value:number,maxDigits:number, maxLength:number=-1 ):string =>{
+    if (value===undefined || isNaN(value))
+        return ''
+
+    if (maxLength===-1)
+        return value.toFixed(maxDigits).toString()
+
+    let digits = maxDigits
+    let str = value.toFixed(maxDigits).toString()
+    while (digits>=0 && str !==undefined && str.length>maxLength+1) {
+        str = value.toFixed(digits).toString()
+        digits--
+    }
+    
+    return str
+
+}
+
+
+
+/**
+ * Pads a number with leading zeros to ensure it has a minimum specified size.
+ * @param {number} value - The number to pad.
+ * @param {number} [size=2] - The minimum size of the padded number. Defaults to 2.
+ * @returns {string} The padded number as a string.
+ */
+export const pad = (value:number,size:number=2) => {
+	let s = value.toString()
+    let s1 = Math.floor(value).toString()
+	while (s1.length < size ) {
+        s = '0' + s;
+        s1 = '0' +s1;
+    }
+	return s;
+}
+
+
---
+++ src/utils/formatting.unit.test.ts
@@ -0,0 +1,102 @@
+import { formatDateTime, formatNumber, formatTime, pad } from "./formatting"
+
+
+describe('formatting',()=>{
+
+    describe('formatDateTime', () => {
+        test('default format string', () => {
+            const date = new Date('2024-02-29T12:34:56');
+            expect(formatDateTime(date)).toBe('20240229123456');
+        });
+    
+        test('with specified format string', () => {
+            const date = new Date('2024-02-29T12:34:56');
+            expect(formatDateTime(date, '%Y-%m-%d %H:%M:%S')).toBe('2024-02-29 12:34:56');
+            expect(formatDateTime(date, '%Y/%m/%d %H:%M:%S')).toBe('2024/02/29 12:34:56');
+            expect(formatDateTime(date, '%Y-%m-%d %H:%M:%S')).toBe('2024-02-29 12:34:56');
+        });
+    
+        test('with UTC methods if utc is true', () => {
+            const date = new Date('2024-02-29T12:34:56Z');
+            
+            expect(formatDateTime(date, '%Y-%m-%d %H:%M:%S', true)).toBe('2024-02-29 12:34:56');
+
+            
+        });
+    
+        test('date is invalid', () => {
+            let invalidDate 
+            expect(formatDateTime(invalidDate)).toBe(undefined);
+        });        
+    });
+
+    describe('formatTime', () => {
+        it('should format time correctly without cutting missing leading hours', () => {
+            expect(formatTime(3661, false)).toBe('1:01:01');
+            expect(formatTime(65, false)).toBe('0:01:05');
+            expect(formatTime(0, false)).toBe('0:00:00');
+            expect(formatTime(7200, false)).toBe('2:00:00');
+            expect(formatTime(36000, false)).toBe('10:00:00');
+        });
+    
+        it('should format time correctly with cutting missing leading hours', () => {
+            expect(formatTime(3661, true)).toBe('1:01:01');
+            expect(formatTime(65, true)).toBe('01:05');
+            expect(formatTime(0, true)).toBe('00:00');
+            expect(formatTime(7200, true)).toBe('2:00:00');
+            expect(formatTime(36000, true)).toBe('10:00:00');
+        });
+    
+        it('should return undefined if seconds is undefined or null', () => {
+            // eslint-disable-next-line prefer-const
+            let x            
+            expect(formatTime(x, false)).toBe(undefined);
+
+            x = null
+            expect(formatTime(x, false)).toBe(undefined);
+        });
+    });
+
+    describe('formatNumber', () => {
+        it('maxDigits and  maxLength ', () => {
+            expect(formatNumber(10.22, 2,4)).toBe('10.22');
+            expect(formatNumber(100.22, 2,4)).toBe('100.2');
+            expect(formatNumber(1000.22, 2,4)).toBe('1000');
+            expect(formatNumber(0.123456, 6, 5)).toBe('0.1235');
+        });
+
+        it('only maxDigits', () => {
+            expect(formatNumber(3.14159, 2)).toBe('3.14');
+            expect(formatNumber(12345.6789, 3)).toBe('12345.679');
+            expect(formatNumber(0.123456, 4)).toBe('0.1235');
+            expect(formatNumber(9876.54321, 0)).toBe('9877');
+        });        
+    })
+
+    describe('pad',()=>{
+        test('padding required',()=>{
+            let res = pad(0.1,2)
+            expect(res).toBe('00.1')
+            res = pad(0,3)
+            expect(res).toBe('000')
+            res = pad(10,3)
+            expect(res).toBe('010')
+        })
+        test('no padding required',()=>{
+            let res = pad(10.1,2)
+            expect(res).toBe('10.1')
+            res = pad(100.1,2)
+            expect(res).toBe('100.1')
+
+        })
+
+        test('mo length provided',()=>{
+            let res = pad(1)
+            expect(res).toBe('01')
+            res = pad(100)
+            expect(res).toBe('100')
+
+        })
+
+    })
+})
