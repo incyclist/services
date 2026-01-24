d5386aed7c866bfaa5b35e75d1666e226dc0d12d
Guido Doumen <incyclist@doumen-laumen.de>
2026-01-24 11:37:48 +0100

    update jest config

--- jest.e2e-config.cjs
+++ jest.e2e-config.cjs
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-let config = require('./jest.config')
+let config = require('./jest.config.cjs')
 config.testRegex = '^[^\\.]+(\\.)?(e2e)\\.(test|spec)\\.(ts|js)?$'
 
 console.log('RUNNING E2E TESTS')
--- jest.unit-config.cjs
+++ jest.unit-config.cjs
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-let config = require('./jest.config')
+let config = require('./jest.config.cjs')
 config.testRegex = '^[^\\.]+(\\.)?(unit)\\.(test|spec)\\.(ts|js)?$'
 
 console.log('RUNNING UNIT TESTS')
