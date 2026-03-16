27ee5c9ce965591ef825510cb446fcb92dffd870
Guido Doumen <git@doumen-laumen.de>
2026-03-16 12:58:10 +0100

    update type exports

--- src/routes/types.ts
+++ src/routes/types.ts
@@ -1,7 +1,9 @@
 
 export type * from './page/types'
 export type * from './list/types'
+export type * from './list/cards/types'
 export type * from './base/api/types'
 export type * from './base/types'
+
 
 export type RouteImportStatus = 'idle' | 'parsing' | 'error' | 'success'
